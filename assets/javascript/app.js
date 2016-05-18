8026adf1ab3111a1125e7f40ba6033ec3794bff9
John Dispennette <jtdartworks@gmail.com>
2016-05-18 12:59:49 -0500

    working on javascript - was able to figure out what my problem was for connecting the two pages - so thats working now

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,17 @@
+// run javascript when after page is loaded
 
+$( document ).ready(function() {
+
+// storing user ID as a variable 
+
+	var userId = $('.userId').val().trim();
+	var gameRef = new Firebase('https://multi-player-rps.firebaseio.com/');
+
+	
+   
+
+$('.signIn').on('click', function(){ }
+
+
+
+)};
